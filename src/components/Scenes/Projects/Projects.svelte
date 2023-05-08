d74a892ab1d03f4a18aff9e0fbcd49ce2756dd78
Enis Budancamanak <Enis.Budancamanak@hotmail.com>
2023-05-08 17:24:25 +0200

    mobile projects sensivity

--- src/components/Scenes/Projects/Projects.svelte
+++ src/components/Scenes/Projects/Projects.svelte
@@ -132,6 +132,8 @@
     const scroller = new VirtualScroll()
     scroller.on((event) => {
       speed = event.deltaY * (isMobile ? 7 : 1)
+      speed = speed > 150 ? 150 : speed < -150 ? -150 : speed
+      console.log(speed)
     })
 
     createPlane()
